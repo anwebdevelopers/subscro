be1cd340efcc69a7a713990ed0f1dbe2db115d20
anwebdevelopers <anwebdev78@gmail.com>
2017-05-21 19:18:36 +0300

    21.05.2017

---
+++ html/js/common.js
@@ -0,0 +1,71 @@
+$(function() {
+
+    'use strict';
+
+    //------------------------------------------------------
+    //Tooltip
+    //------------------------------------------------------
+    $('[data-toggle="tooltip"]').tooltip({
+        delay: {
+            'show': 200,
+            'hide': 200
+        }
+    });
+
+    //------------------------------------------------------
+    //Datetimepicker
+    //------------------------------------------------------
+
+    $('[data-toggle="datetimepicker"]').datetimepicker({
+      pickTime: false
+    });
+    $('.bootstrap-datetimepicker-widget').on('click', '.day', function() {
+        $('.bootstrap-datetimepicker-widget').hide();
+    });
+
+    //------------------------------------------------------
+    //Toggle End Date Wrapper
+    //------------------------------------------------------
+    $('#end-date-wrapper').hide();
+    $('#end-date-toggler').click(function() {
+        $('#end-date-wrapper').slideToggle(200);
+    });
+
+    //------------------------------------------------------
+    //Chrome Smooth Scroll
+    //------------------------------------------------------
+    try {
+        $.browserSelector();
+        if ($('html').hasClass('chrome')) {
+            $.smoothScroll();
+        }
+    } catch (err) {
+
+    };
+
+    $('img, a').on('dragstart', function(event) {
+        event.preventDefault();
+    });
+
+    //------------------------------------------------------
+    //Full height body for IE
+    //------------------------------------------------------
+
+    var $body = $('body');
+    function fullheight() {
+        if($('html').hasClass('ie') || $('html').hasClass('gecko')) {
+            $body.removeAttr('style');
+            var windowHeight = $(window).height(),
+                bodyHeight = $body.height();
+            if (windowHeight >= bodyHeight) {
+                $body.css({
+                    'height' : windowHeight + 'px'
+                });
+            }
+        }
+    }
+    fullheight();
+    $(window).resize(function() {
+        fullheight();
+    });
+});
